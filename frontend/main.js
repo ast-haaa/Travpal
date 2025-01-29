ecda56347abf8f768af62b3fb4e646e0e6a89b80
MaasFa <khanmasfah@gmail.com>
2025-01-29 18:20:18 +0530

    updated

--- frontend/main.js
+++ frontend/main.js
@@ -4,7 +4,7 @@ const scrollRevealOption = {
   duration: 1000,
 };
 
-// header container
+
 ScrollReveal().reveal(".header__container h1", scrollRevealOption);
 
 ScrollReveal().reveal(".header__container h4", {
@@ -17,7 +17,7 @@ ScrollReveal().reveal(".header__container .btn", {
   delay: 1000,
 });
 
-// about container
+
 ScrollReveal().reveal(".about__container .section__header", scrollRevealOption);
 ScrollReveal().reveal(".about__container .section__subheader", {
   ...scrollRevealOption,
@@ -34,7 +34,7 @@ ScrollReveal().reveal(".about__container .btn", {
   delay: 1500,
 });
 
-// discover container
+
 ScrollReveal().reveal(".discover__card", {
   ...scrollRevealOption,
   interval: 500,
@@ -46,13 +46,12 @@ ScrollReveal().reveal(".discover__card__content", {
   delay: 200,
 });
 
-// blogs container
+
 ScrollReveal().reveal(".blogs__card", {
   duration: 1000,
   interval: 400,
 });
 
-// journals container
 ScrollReveal().reveal(".journals__card", {
   ...scrollRevealOption,
   interval: 400,
